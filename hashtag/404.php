6d2f8c69fc13159cf2f5c04748551178603268e4
Alan Aumiller II <alan2etc@gmail.com>
2020-01-25 00:12:13 -0500

    Update
    Updated 404.php, screenshot.png and style.css

--- hashtag/404.php
+++ hashtag/404.php
@@ -10,25 +10,66 @@
 get_header();
 ?>
 
+<head>
+<style>
+body{
+	background-color: black;
+}
+.site-header{
+	font-size: x-large;
+	font-wieght: bold;
+}
+h1,p { 
+	color: white;
+}
+.page-content{
+	padding-top: 100px;
+	padding-bottom: 250px;	
+	padding-left: 30%;
+	text-align: center;
+}
+.page-title{
+	text-align: center;
+	padding-left: 30%;
+}
+.search-form{
+	text-align: center;
+}
+.image {
+    position: relative;
+    top: 10%;
+    left: 10%;
+    margin:-60px 0 0 -60px;
+    -webkit-animation:spin 4s linear infinite;
+    -moz-animation:spin 4s linear infinite;
+    animation:spin 4s linear infinite;
+}
+</style>
+</head>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-			<section class="error-404 not-found">
+			<section class="error">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'hashtag' ); ?></h1>
+					<h1 class="page-title"><?php esc_html_e( 'Error Message: 404', 'hashtag' ); ?></h1>
 				</header><!-- .page-header -->
-
+				
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'hashtag' ); ?></p>
-
+				<img class="image" src= "/aaumiller66/wordpress/wp-content/themes/hashtag/assets/nothing.jpeg">
+				<br>
+				<br>
+				<br>
+					<p><?php esc_html_e( 'Nothing was found at this location. Try searching below.', 'hashtag' ); ?></p>
+						
 					<?php
 					get_search_form();
 
-					the_widget( 'WP_Widget_Recent_Posts' );
+					// the_widget( 'WP_Widget_Recent_Posts' );
 					?>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'hashtag' ); ?></h2>
+					<!-- <div class="widget widget_categories"> -->
+						<!-- <h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'hashtag' ); ?></h2>
 						<ul>
 							<?php
 							wp_list_categories( array(
@@ -39,20 +80,19 @@ get_header();
 								'number'     => 10,
 							) );
 							?>
-						</ul>
-					</div><!-- .widget -->
+						</ul> -->
+					<!-- </div>.widget -->
 
 					<?php
 					/* translators: %1$s: smiley */
-					$hashtag_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'hashtag' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$hashtag_archive_content" );
+					// $hashtag_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'hashtag' ), convert_smilies( ':)' ) ) . '</p>';
+					// the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$hashtag_archive_content" );
 
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+					// the_widget( 'WP_Widget_Tag_Cloud' );
+					// ?>
 
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
-
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
